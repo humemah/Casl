74d14f4f43634fdfb51287b4ec1f5d487dff0704
Humemah <Humemah.Khalid@csquare.co>
2023-07-31 12:48:09 +0500

    Initial commit

--- src/components/Ability.js
+++ src/components/Ability.js
@@ -3,6 +3,5 @@ import { defineAbility } from '@casl/ability';
 export default defineAbility((can, cannot) => {
   can('read', 'Todo');
   can('update', 'Todo');
-  // can('delete', 'Todo');
   cannot('delete', 'Todo', );
 });
